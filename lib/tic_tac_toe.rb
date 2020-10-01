64eb4a17ebbc8e34c5ca9bcbc2158f3f5c9f7db1
Liselle Logan <liselle.logan@gmail.com>
2020-10-01 01:44:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,7 +96,16 @@ class TicTacToe
   end
 
   def play
-    
+    until the game is over
+  take turns
+end
+
+ def play
+# if the game was won
+#   congratulate the winner
+# else if the game was a draw
+#   tell the players it ended in a draw
+# end
   end
 
 end
